29ce9054e91a791b53babca41172ca18fb7f8838
Vimalate <1902644369@qq.com>
2023-04-09 15:27:55 +0800

    配置监听文件启动类型

--- electron-demo/main.js
+++ electron-demo/main.js
@@ -5,6 +5,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
     height: 600,
+    // frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
